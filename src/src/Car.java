774ed6f8a831de14da8bfc7d700139ef57a74221
charan-sri <103118436+charan-sri@users.noreply.github.com>
2022-06-25 16:09:46 +0530

    added all the features

--- src/src/Car.java
+++ src/src/Car.java
@@ -1,5 +1,8 @@
+import java.sql.Time;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 interface car1 {
     abstract void Startup();
@@ -18,14 +21,12 @@ class Start implements car1 {
             System.out.println("1.StartEngine()");
             System.out.println("2.AccessoryMode()");
             System.out.println("3.IgnitionMode()");
-            System.out.println("enter about all parts");
-            parts = s.next();
             System.out.println("Select an option: ");
-
             a = s.nextInt();
             switch (a) {
                 case 1:
                     System.out.println("Welcome to MG.Vehicle check-OK.Wear your seat bealt and drive safe!!");
+                    drivemode();
                     break;
                 case 2:
                     System.out.println("Accessory mode.You can use the accesories.");
@@ -38,6 +39,7 @@ class Start implements car1 {
                 default:
                     System.out.println("Apply the brake and push the ignition button to start the engine!!");
             }
+            i--;
         }
     }
 
@@ -45,26 +47,25 @@ class Start implements car1 {
      * 100=Everything is working.
      * 200=Major funtionality working.There is some fault.
      * 300=Faulty vehicle functionality.
+     *
      * @return
      */
     public int getVehicleStatus() {
-      int status[]=new int[]{100,200,300};
-        Random rand= new Random();
+        int status[] = new int[]{100, 200, 300};
+        Random rand = new Random();
         return status[rand.nextInt(3)];
 
     }
-    public void useAccessories()
-    {
-        int vehicleStatus=getVehicleStatus();
-        if(vehicleStatus==100)
-        {
+
+    public void useAccessories() {
+        int vehicleStatus = getVehicleStatus();
+        if (vehicleStatus == 100) {
             System.out.println("We are good to go.");
             checkStatus(100);
-        } else if (vehicleStatus==200) {
+        } else if (vehicleStatus == 200) {
             System.out.println("You may face some inconvinience.Please check.");
             checkStatus(200);
-        }
-        else{
+        } else {
             System.out.println("Faulty Engine.");
             checkStatus(-1);
         }
@@ -81,8 +82,115 @@ class Start implements car1 {
             System.out.println("Faulty Engine.");
         }
     }
+
+    public void drivemode() {
+        int i = 0, c;
+
+        while (i == 0) {
+            Scanner s1 = new Scanner(System.in);
+            System.out.println("1.SportMode()");
+            System.out.println("2.AWD mode()");
+            System.out.println("3.Reverse()");
+            System.out.println("4.EcoMode()");
+
+            System.out.println("Drive mode selection: ");
+            c = s1.nextInt();
+            switch (c) {
+                case 1:
+                    System.out.println("Wear seatbelt and donot overspeed.");
+                    getBattery();
+                    break;
+                case 2:
+                    System.out.println("Use this mode for hillstart,consumes more power look after the battery percentage.");
+                    getBattery();
+                    break;
+                case 3:
+                    System.out.println("Rear camera turned on,also check the surroundings.");
+                    getBattery();
+                    break;
+                case 4:
+                    System.out.println("Economy mode gives more range.Drive longer and safer");
+                    getBattery();
+                default:
+                    System.out.println("Exit ");
+            }
+            i--;
+
+        }
+    }
+        public void transactions(){
+
+            Timer timer = new Timer();
+            timer.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    getBattery();
+                }
+            },5000,1000);
+        }
+int b1,b2;
+    public void getBattery() {
+
+
+        int battery1[] = new int[101];
+        for (int a = 0; a <= 100; a++) {
+            battery1[a] = a;
+        }
+        Random rand = new Random();
+        b1 = battery1[rand.nextInt(101)];
+        System.out.println("b1= " + b1);
+        int battery2[] = new int[101];
+        for (int a = 0; a <= 100; a++) {
+            battery2[a] = a;
+        }
+        b2 = battery2[rand.nextInt(101)];
+        System.out.println("b2= " + b2);
+    }
+
+
+
+
+   public void switching()
+   {
+
+       while(b1!=0 || b2!=0)
+       {
+           if(b1>b2)
+           {
+               b1-=5;
+               b2+=2;
+               System.out.println("b1 is discharging and b2 is recharging");
+               System.out.println("b1="+b1);
+               System.out.println("b2="+b2);
+           }
+           else if (b2>b1)
+           {
+            b2-=5;
+            b1+=2;
+               System.out.println("b2 is discharging and b1 is recharging");
+               System.out.println("b1="+b1);
+               System.out.println("b2="+b2);
+
+           }
+           else if (b1==b2)
+           {
+               b1-=5;
+               b2+=2;
+               System.out.println("b1 is discharging and b2 is recharging");
+               System.out.println("b1="+b1);
+               System.out.println("b2="+b2);
+           }
+           break;
+       }
+   }
+
+
     public static void main(String args[]) {
         Start obj = new Start();
         obj.Startup();
+        obj.switching();
+
+
+
     }
 }
